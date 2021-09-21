d5d94f34e894b9b6f0fa227fecb6a98b0e5c5537
Shelley Fong <s.fong@auckland.ac.nz>
2021-09-22 09:14:44 +1200

    Check for suffixes not accounted for

--- omex_manifest_maker.py
+++ omex_manifest_maker.py
@@ -1,4 +1,5 @@
 # Create a manifest file for an omex archive. Specify the path of the contents (i.e. the parent directory)
+# Output: new_manifest.xml (rename to manifest.xml before putting into OMEX archive)
 # Author: Shelley Fong
 
 import os
@@ -9,6 +10,7 @@ path = "submission_files/S000007/"
 # also check subfolders
 f = [os.path.join(dp.split(path)[-1], f).replace('\\','/') for dp, dn, filenames in os.walk(path) for f in filenames]
 # f = os.listdir(path)
+f = [i for i in f if not i.endswith('manifest.xml')]
 
 fclml = [i for i in f if i.endswith(".cellml")]
 fsed = [i for i in f if i.endswith(".sedml")]
@@ -18,63 +20,40 @@ fmat = [i for i in f if i.endswith(".m")]
 fxlsx = [i for i in f if i.endswith(".xlsx")]
 fpdf = [i for i in f if i.endswith(".pdf")]
 
+# check if any suffix is not accounted for
+assert len(fclml + fsed + fpy + fcsv + fmat + fxlsx + fpdf) == len(f)
+
 # write to manifest
 outfile = path + "new_manifest.xml"
-if True:
-    data = ET.Element("omexManifest")
-    data.set("xmlns","https://identifiers.org/combine.specifications/omex-manifest")
-    content = ET.SubElement(data, "content")
-    content2 = ET.SubElement(data, "content")
-    content.set("location", ".")
-    content.set("format", "https://identifiers.org/combine.specifications/omex")
-    content2.set("location", "./manifest.xml")
-    content2.set("format", "https://identifiers.org/combine.specifications/omex-manifest")
+data = ET.Element("omexManifest")
+data.set("xmlns","https://identifiers.org/combine.specifications/omex-manifest")
+content = ET.SubElement(data, "content")
+content2 = ET.SubElement(data, "content")
+content.set("location", ".")
+content.set("format", "https://identifiers.org/combine.specifications/omex")
+content2.set("location", "./manifest.xml")
+content2.set("format", "https://identifiers.org/combine.specifications/omex-manifest")
 
-    # write out cellml files followed by sedml files
-    for cname in fclml:
-        contentx = ET.SubElement(data, "content")
-        contentx.set("location", "./"+cname)
+# write out cellml files followed by sedml files etc
+for cname in f:
+    contentx = ET.SubElement(data, "content")
+    contentx.set("location", "./" + cname)
+    if cname in fclml:
         contentx.set("format", "http://identifiers.org/combine.specifications/cellml")
-    for cname in fsed:
-        contentx = ET.SubElement(data, "content")
-        contentx.set("location", "./"+cname)
+    if cname in fsed:
         contentx.set("format", "http://identifiers.org/combine.specifications/sed-ml")
-    for cname in fpy:
-        contentx = ET.SubElement(data, "content")
-        contentx.set("location", "./"+cname)
+    if cname in fpy:
         contentx.set("format", "application/x-python")
-    for cname in fcsv:
-        contentx = ET.SubElement(data, "content")
-        contentx.set("location", "./"+cname)
+    if cname in fcsv:
         contentx.set("format", "text/csv")
-    for cname in fmat:
-        contentx = ET.SubElement(data, "content")
-        contentx.set("location", "./"+cname)
+    if cname in fmat:
         contentx.set("format", "text/x-matlab")
-    for cname in fxlsx:
-        contentx = ET.SubElement(data, "content")
-        contentx.set("location", "./"+cname)
+    if cname in fxlsx:
         contentx.set("format", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-    for cname in fxlsx:
-        contentx = ET.SubElement(data, "content")
-        contentx.set("location", "./"+cname)
+    if cname in fpdf:
         contentx.set("format", "application/pdf")
 
-    # content.text = "ffffffff"
+# content.text = "ffffffff"
 
-    tree = ET.ElementTree(data)
-    tree.write(outfile, encoding="utf-8", xml_declaration=True, pretty_print=True)
-else:
-    with open(outfile,"w") as of:
-        of.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-        of.write('<omexManifest xmlns="https://identifiers.org/combine.specifications/omex-manifest">\n')
-        of.write('  <content location="." format="https://identifiers.org/combine.specifications/omex" />\n')
-        of.write('  <content location="./manifest.xml" format="https://identifiers.org/combine.specifications/omex-manifest" />\n')
-
-        # write out cellml files followed by sedml files
-        for cname in fclml:
-            of.write('  <content location="./" + cname + "" format="http://identifiers.org/combine.specifications/cellml" />\n')
-
-        of.close()
-
-j = 10
+tree = ET.ElementTree(data)
+tree.write(outfile, encoding="utf-8", xml_declaration=True, pretty_print=True)
