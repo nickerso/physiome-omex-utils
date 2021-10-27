ca18be8ba77f44134f192410d92ccc3d9b42fa5f
Shelley Fong <s.fong@auckland.ac.nz>
2021-10-27 13:57:09 +1300

    Adding accepted MIME types

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # OMEX_manifest_maker
 
 Prerequisites: lxml (pip install)
+
+## list of supported MIME types:
+.cellml
+.sedml
+.python
+.csv
+.m
+.xlsx
+.pdf
+Fork this repository then do a git pull request to add new MIME types or to make changes.
--- omex_manifest_maker.py
+++ omex_manifest_maker.py
@@ -2,10 +2,20 @@
 # Output: new_manifest.xml (rename to manifest.xml before putting into OMEX archive)
 # Author: Shelley Fong
 
+# list of supported MIME types:
+# .cellml
+# .sedml
+# .python
+# .csv
+# .m
+# .xlsx
+# .pdf
+# Fork this repository then do a git pull request to add new MIME types or to make changes.
+
 import os
 import lxml.etree as ET
 
-path = "submission_files/S000007/"
+path = "C:/Users/sfon036/Desktop/work_files/PhysiomeSubmissions/Ai2021PhysiomeS000013_27Oct/" # "submission_files/S000007/"
 
 # also check subfolders
 f = [os.path.join(dp.split(path)[-1], f).replace('\\','/') for dp, dn, filenames in os.walk(path) for f in filenames]
@@ -20,8 +30,8 @@ fmat = [i for i in f if i.endswith(".m")]
 fxlsx = [i for i in f if i.endswith(".xlsx")]
 fpdf = [i for i in f if i.endswith(".pdf")]
 
-# check if any suffix is not accounted for
-assert len(fclml + fsed + fpy + fcsv + fmat + fxlsx + fpdf) == len(f)
+# check if any MIME type (suffix) is not accounted for
+assert len(fclml + fsed + fpy + fcsv + fmat + fxlsx + fpdf) == len(f), "Filetype present which not on list of supported MIME types"
 
 # write to manifest
 outfile = path + "new_manifest.xml"
@@ -52,8 +62,6 @@ for cname in f:
         contentx.set("format", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
     if cname in fpdf:
         contentx.set("format", "application/pdf")
-
-# content.text = "ffffffff"
 
 tree = ET.ElementTree(data)
 tree.write(outfile, encoding="utf-8", xml_declaration=True, pretty_print=True)
